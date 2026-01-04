2ee162d72517e147db0328ac535a2d44d3b18ed3
TriaNoya12 <23421150@student.ubl.ac.id>
2026-01-04 21:39:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+from chatbot import get_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_msg = request.json["message"]
+    bot_msg = get_response(user_msg)
+    return jsonify({"reply": bot_msg})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
